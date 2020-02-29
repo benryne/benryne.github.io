7889f5369c77aec9524735c46d30a611c7d98944
benryne <bryne@g.clemson.edu>
2020-02-29 14:37:36 -0600

    accordian testing

--- src/app.js
+++ src/app.js
@@ -2,15 +2,21 @@ import 'bootstrap';
 import './scss/style.scss'
 import $ from 'jquery';
 
+let last = null;
+
 $( document ).ready(function() {
     $('.collapse').collapse()
 
     $( ".skills-button" ).click(function() {
+        console.log($(this));
         console.log("clicked");
     });
 
-    $('#skillsAccordion').on('hidden.bs.collapse', function () {
+    $('').on('hidden.bs.collapse', function () {
         console.log("closed from accordian");
+        last.toggleClass()
         console.log($(this));
-      })
+    })
+
+
 });
